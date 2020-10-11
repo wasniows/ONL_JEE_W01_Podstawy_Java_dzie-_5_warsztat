2cadc27f84b678c41eeff96450e895485fa07807
wojtek <wasniows@gmail.com>
2020-10-11 15:51:48 +0200

    final version

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,42 +1,58 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class TaskManager {
-static final String FILE_NAME = "tasks.csv";
+    static final String FILE_NAME = "tasks.csv";
+    static String[][] tasks;
 
     public static void main(String[] args) {
+        tasks = loadFromFile(FILE_NAME);
+        options();
 
         Scanner scanner = new Scanner(System.in);
-            while (scanner.hasNextLine()){
-                String input = scanner.nextLine();
-                switch (input){
-                    case "exit":
-                        break;
-                    case "add":
-                        break;
-                    case "remove":
-                        break;
-                    case "list":
-                        printArray(tasks());
-                    default:
-                        System.out.println("Please select a correct option");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "exit" -> {
+                    saveTabToFile(FILE_NAME, tasks);
+                    System.exit(0);
                 }
-                options();
+                case "add" -> {
+                    addTask();
+                    System.out.println("New task was added");
+                    System.out.println();
+                }
+                case "remove" -> {
+                    removeTask(tasks, getNubmer());
+                    System.out.println("Task was deleted");
+                    System.out.println();
+                }
+                case "list" -> {
+                    System.out.println();
+                    printArray(tasks);
+                    System.out.println();
+                }
+                default -> {
+                    System.out.println("Please select correct option");
+                    System.out.println();
+                }
             }
+            options();
+        }
 
-        //options();
-        System.out.println(Arrays.deepToString(tasks(FILE_NAME)));
     }
 
-    public static void options (){
+    //metoda do wyświetlania listy dostpęnych opcji
+    public static void options() {
         System.out.println(ConsoleColors.GREEN_BOLD + "- add");
         System.out.println(ConsoleColors.RED + "- remove");
         System.out.println(ConsoleColors.BLUE + "- list");
@@ -45,38 +61,135 @@ static final String FILE_NAME = "tasks.csv";
         System.out.println("Please select an option");
     }
 
-    public static String [][] tasks (String fileName){
+    //motoda do wczytania wszystich zadań w pliku
+    public static String[][] loadFromFile(String fileName) {
 
         Path dir = Paths.get(fileName);
-        if(!Files.exists(dir)){
+        if (!Files.exists(dir)) {
             System.out.println("File not exist");
+            System.exit(0);
         }
 
-        String [][] array = null;
+        String[][] array = new String[0][0];
 
         try {
-            List<String> strings = Files.readAllLines(dir);
-            array = new String[strings.size()][strings.get(0).split(",").length];
+            File file = new File(fileName);
+            int i = 0;
+            String line;
+            String[] splited;
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
 
-            for (int i = 0; i < strings.size(); i++) {
-                String[] split = strings.get(i).split(",");
-                for (int j = 0; j < split.length; j++) {
-                    array[i][j] = split[j];
+                if (line == null) {
+                    System.out.println("Blank list");
+                } else {
+                    splited = line.split(",");
+                    array = Arrays.copyOf(array, array.length + 1);
+                    array[array.length - 1] = new String[splited.length];
+
+                    System.arraycopy(splited, 0, array[i], 0, splited.length);
+                    i = i + 1;
                 }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }return array;
+        }
+        return array;
     }
 
-    public static void printArray (String [][] Array){
-        for (int i = 0; i < Array.length; i++){
-            System.out.print((i + " : "));
-            for (int j = 0; j < Array.length; j++) {
-                System.out.print(Array[i][j] + " ");
+
+
+    //metoda do wyświetlenia aktualnej listy zadań
+    public static void printArray(String[][] Array) {
+
+        if (Array == null) {
+            System.out.println("Blank list");
+        } else {
+            for (int i = 0; i < Array.length; i++) {
+                System.out.print(i + " : ");
+                for (int j = 0; j < Array[i].length; j++) {
+                    System.out.print(Array[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 
+    //dodawanie kolejnego zadania do listy
+    private static void addTask() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add task description");
+        String description = scanner.nextLine();
+        System.out.println("Please add task due date");
+        String dueDate = scanner.nextLine();
+        System.out.println("Is your task important: true/false");
+        String isImportant = scanner.nextLine();
+
+        if (tasks == null) {
+            tasks = new String[0][0];
+        }
+
+        tasks = Arrays.copyOf(tasks, tasks.length + 1);
+        tasks[tasks.length - 1] = new String[3];
+        tasks[tasks.length - 1][0] = description;
+        tasks[tasks.length - 1][1] = dueDate;
+        tasks[tasks.length - 1][2] = isImportant;
+
+    }
+
+    //sprawdzenie czy podana liczba jest >= zero
+
+    public static boolean isNumberGreatEqualZero(String input) {
+        if (NumberUtils.isCreatable(input)) {
+            return Integer.parseInt(input) >= 0;
+        }
+        return false;
+    }
+
+    //wczytanie z konsoli numeru zadania do usunięcia
+    public static int getNubmer() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select number to remove");
+
+        String n = scanner.nextLine();
+        while (!isNumberGreatEqualZero(n)) {
+            System.out.println("Incorrect argument");
+            scanner.nextLine();
+        }
+        return Integer.parseInt(n);
+    }
+
+    //usuniecie zadania z listy
+    private static void removeTask(String[][] tab, int index) {
+        try {
+            if (index < tab.length) {
+                tasks = ArrayUtils.remove(tab, index);
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Element not exist");
+        }
+    }
+
+    //zapisanie aktualnej listy zadań do pliku
+    public static void saveTabToFile(String fileName, String[][] tab) {
+        Path dir = Paths.get(fileName);
+
+        if (tab == null) {
+            System.out.println("Nothing to save");
+        } else {
+            String[] lines = new String[tasks.length];
+            for (int i = 0; i < tab.length; i++) {
+                lines[i] = String.join(",", tab[i]);
+            }
+
+            try {
+                Files.write(dir, Arrays.asList(lines));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("Saved");
+        }
+    }
 }
